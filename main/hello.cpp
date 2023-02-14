4bc057be61a84991ef01d3b5981b309595ac8bf4
unknown <suraj.gautham@gmail.com>
2023-02-14 22:36:24 +0530

    First commit CloudComputingLab

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This assignment takes too long :(";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
