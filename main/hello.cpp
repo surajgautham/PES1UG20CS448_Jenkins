0da7a5ac1310af5c760b06374914aa60c472549f
suraj r gautham <34979441+surajgautham@users.noreply.github.com>
2023-02-17 11:15:53 +0530

    Webhook commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "This assignment takes too long :(";
+  cout << "This assignment takes too long :(\n";
   cout << "I have successfully built and run\n";
+  cout << "I have made a change and will obtain result through webhook\n";
   return 0;
 }
